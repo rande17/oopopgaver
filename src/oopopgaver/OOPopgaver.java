0f7bf06bba182d8c122b49631daffe25a2ab72dd
Rickie <rande17@student.sdu.dk>
2017-09-26 16:57:48 +0200

    cleaning code

--- src/oopopgaver/OOPopgaver.java
+++ src/oopopgaver/OOPopgaver.java
@@ -5,22 +5,49 @@
  */
 package oopopgaver;
 
+import java.util.Scanner;
+
 /**
  *
  * @author clutt
  */
 public class OOPopgaver {
 
+    static boolean bob = true;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        //make an instance of the Investment class
-        Investment invest = new Investment();
-        
-        //using the methods in invest to print out the result
-        System.out.println(invest.futureInvestmentValue(invest.getInvestDouble(), invest.getIntrestDouble(), invest.getYearsInt()));
+        while (bob) {
+            chooseTask();
+        }
+
+    }
+
+    public static void chooseTask() {
+        String o;
+        System.out.print("Select task: ");
+        Scanner iScn = new Scanner(System.in);
+        if (iScn.hasNext()) {
+            o = iScn.next();
+            switch (o) {
+                case "1":
+                    //make an instance of the Investment class
+                    Investment invest = new Investment();
+                    //using the methods in invest to print out the result
+                    System.out.println(invest.futureInvestmentValue(invest.getInvestDouble(), invest.getIntrestDouble(), invest.getYearsInt()));
+                    break;
+                case "q":
+                case "quit":
+                case "exit":
+                    bob = false;
+                    break;
+                    
+            }
+
+        }
+
     }
 
 }
